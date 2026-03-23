a0de8132fd8c4cf54a88be54b777bbb9d63afb3c
Даша <dasha@MacBook-Air-Dasa.local>
2026-03-23 11:22:26 +0300

    семинар 10 задачи

---
+++ seminar10.py
@@ -0,0 +1,57 @@
+"""st = input().split(".")
+for i in range(len(st)):
+    st[i]= int(st[i])
+
+for i in range(len(st)):
+    if 0 <= int(st[i]) <= 225:
+        count+=1
+
+if count == 4:
+    print("Да")
+else:
+    print("Нет")
+    
+
+s= input().split()
+print(*s, sep = "\n")
+
+s = int(input()).split()
+i = 3
+j = 4
+counter=0
+for i in range(len(s)):
+    for i in range(i+1, len(s)):
+        if s[i]==s[j]:
+         counter+=1
+print(counter)
+
+    #numbers = [выражение for переменная цикла in range(...)]
+lst = []
+for i in "jfuirfirj":
+    lst.append(i)
+print(lst)
+
+
+
+print(input().lower()for _ in range(int(input())))
+print([i**2 for i in range(10)if (i**2)%10 !=4])
+
+
+n = int(input())
+print(*[i**2 for i in range(1,n+1)], sep="\n")
+
+
+
+
+
+print(*[i**2 for i in range(1, int(input()+1))], sep ="\n")
+lst=[]
+
+for i in range(1, int(input())+1)
+    lst.append(1**2)
+
+
+
+
+print(*[i for i in list(input()) if i in "0123456789"], sep=" ")"""
+
